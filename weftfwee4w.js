7f8689d7ba3713032ae81acb276847016e93d6e0
Ethan De Jesus <124072155+magicianZ@users.noreply.github.com>
2023-10-30 13:13:38 -0400

    mjk,,k

--- weftfwee4w.js
+++ weftfwee4w.js
@@ -5,8 +5,10 @@ const DOMSelectors = {
     button:document.getElementById("btn"),
     blackbox:document.getElementById("blackbox"),
     text: document.querySelector("#text"),
-    input:document.querySelector("input"),
-    input1:document.querySelector("#input")
+    attunementinput:document.querySelector("input"),
+    mantrainput:document.querySelector("#input1"),
+    starinput:document.querySelector("#input2"),
+    remove:document.querySelector("#removebtn")
 
 }
 
@@ -24,15 +26,24 @@ function backgroundtext(background,text){
 
 
 
+
+
+
+
 DOMSelectors.button.addEventListener("click",function(){
-    let input = DOMSelectors.input.value;
-    let input1 = DOMSelectors.input1.value;
-    console.log(input)
+    let input = DOMSelectors.attunementinput.value;
+    let input1 = DOMSelectors.mantrainput.value;
+    let input2 = DOMSelectors.starinput.value;
+
+
+
     DOMSelectors.blackbox.insertAdjacentHTML("beforeend",
     `
     <div id="bocks"
         <p> ${input}</p>
         <p>${input1}</p>
+        <img src="${input2}" alt="mantrea" width="200" height="200">
+        <button type="button" id="removebtn">remove i think</button>
     
     
     `
@@ -40,3 +51,12 @@ DOMSelectors.button.addEventListener("click",function(){
     
     
     )})
+
+function remove(){
+    let remove = document.querySelector("#removebutton")
+    remove.forEach((btn)=>
+    btn.addEventListener("click",function(event){
+        console.log(event.target.parentElement)
+    }))
+    }
+remove()
